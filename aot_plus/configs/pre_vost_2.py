d84121fb523f81e519beb8e864c2bd3d0f1a4c7a
bowmanchow <bowmanchow@gmail.com>
2023-09-10 23:59:31 +0800

    change clip length to 17 in pre_vost_2

--- aot_plus/configs/pre_vost_2.py
+++ aot_plus/configs/pre_vost_2.py
@@ -11,7 +11,7 @@ class EngineConfig(DefaultEngineConfig):
 
         self.DATASETS = ['vost']
         self.TRAIN_TOTAL_STEPS = 40000
-        self.DATA_SEQ_LEN = 25
+        self.DATA_SEQ_LEN = 17
         self.TRAIN_LONG_TERM_MEM_GAP = 4 if not (hasattr(self, "NO_MEMORY_GAP") and self.NO_MEMORY_GAP) else 1
         self.MODEL_LINEAR_Q = False
         self.MODEL_IGNORE_TOKEN = True
