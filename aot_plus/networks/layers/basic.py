283d7a150e347895744c51a8e33875905ce59add
bowmanchow <bowmanchow@gmail.com>
2023-11-15 12:48:01 -0600

    add deaot's attention and DWConv

--- aot_plus/networks/layers/basic.py
+++ aot_plus/networks/layers/basic.py
@@ -35,6 +35,28 @@ class GNActDWConv2d(nn.Module):
         return x
 
 
+class DWConv2d(nn.Module):
+    def __init__(self, indim, dropout=0.1):
+        super().__init__()
+        self.conv = nn.Conv2d(indim,
+                              indim,
+                              5,
+                              dilation=1,
+                              padding=2,
+                              groups=indim,
+                              bias=False)
+        self.dropout = nn.Dropout2d(p=dropout, inplace=True)
+
+    def forward(self, x, size_2d):
+        h, w = size_2d
+        _, bs, c = x.size()
+        x = x.view(h, w, bs, c).permute(2, 3, 0, 1)
+        x = self.conv(x)
+        x = self.dropout(x)
+        x = x.view(bs, c, h * w).permute(2, 0, 1)
+        return x
+
+
 class ConvGN(nn.Module):
     def __init__(self, indim, outdim, kernel_size, gn_groups=8):
         super().__init__()
